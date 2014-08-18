8357a54b96cc027ef5e3c069ca61981a876adc8f
Mikhail <MV@Vlads-Air.Dlink>
2014-08-18 14:36:56 +0300

    Initial Commit

---
+++ HomeWorkArrays_5/MVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MVAppDelegate.h
+//  HomeWorkArrays_5
+//
+//  Created by Mikhail  on 18.08.14.
+//  Copyright (c) 2014 Mikhail Vilenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeWorkArrays_5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomeWorkArrays_5
+//
+//  Created by Mikhail  on 18.08.14.
+//  Copyright (c) 2014 Mikhail Vilenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MVAppDelegate class]));
+    }
+}
